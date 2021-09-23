6835401b7c0b0175de7a46a38ada02cbb2d9aed4
backcoc <yryousafraza@gmail.com>
2021-09-23 20:06:18 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+// Requiring express in our server
+const express = require('express');
+const app = express();
+var jsonfile = require('jsonfile');    
+var file = './db.json'
+var filex = require('./db.json')
+app.engine('html', require('ejs').renderFile);
+
+const bp = require('body-parser')
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
+
+app.set('view engine', 'html')
+// Defining get request at '/' route
+app.get('/', function(req, res) {
+
+    res.send("<html><head><title>Json</title></head><body><form id='form1' action='/gettingdata' method='post'><input type='text' name='usrid' /><button type='submit' form='form1' value='Submit'>Submit</button></form></body></html>")
+});
+app.post('/gettingdata',function(req,res){
+    
+    var user_id = req.body.usrid;
+    
+    var obj = JSON.parse(user_id)
+    jsonfile.writeFileSync(file, obj,{flag: 'w'});
+    res.send('updated');
+
+})
+app.post('/api',function(req,res){
+    res.send(filex)
+})
+// if(user_id){
+//     var user= user_id
+// }
+// app.post('/api',function(req,res,user){
+//     res.send(user)
+// })
+// Defining get request at '/multiple' route
+app.get('/multiple', function(req, res) {
+res.json({
+	number: 1,
+	name: 'John',
+	gender: 'male'
+});
+});
+
+// Defining get request at '/array' route
+app.get('/array', function(req, res) {
+res.json([{
+	number: 1,
+	name: 'John',
+	gender: 'male'
+	},
+	{
+	number: 2,
+	name: 'Ashley',
+	gender: 'female'
+	}
+]);
+});
+
+// Setting the server to listen at port 3000
+app.listen(3000, function(req, res) {
+console.log("Server is running at port 3000");
+});
